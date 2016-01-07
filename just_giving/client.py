81e0e6a139b8584f14ecf5242e32ffc3eeab2695
jmz <jmz-b@users.noreply.github.com>
2016-01-07 16:31:44 +0000

    retab, raise_for_status on post

--- just_giving/client.py
+++ just_giving/client.py
@@ -44,18 +44,28 @@ class BaseAPIClient(object):
         else:
             content_text = 'text/xml'
         if self.authentication_code:
-            headers = {'Content-Type': content_text,
-                       'Authorization': 'Basic ' + self.authentication_code}
+            headers = {
+                'Content-Type': content_text,
+                'Authorization': 'Basic ' + self.authentication_code
+            }
         else:
             headers = {'Content-Type': content_text}
         self.headers = headers
 
     def get(self):
-        return requests.get(self.build_url(), headers=self.headers)
+        response = requests.get(self.build_url(), headers=self.headers)
+        response.raise_for_status()
+        return response.json()
 
     def post(self, data):
-        return requests.post(self.build_url(),
-                             headers=self.headers, data=json.dumps(data))
+        response = requests.post(
+            self.build_url(),
+            headers=self.headers,
+            data=json.dumps(data)
+        )
+        response.raise_for_status()
+        return response.json()
+
 
     def head(self):
         return requests.head(self.build_url(), headers=self.headers)
@@ -66,22 +76,22 @@ class AccountAPIClient(BaseAPIClient):
     def get_fundraising_pages_for_user(self, email):
         self.api_endpoint = '/[appId]/[apiVersion]/account/{0}/pages'.format(
             email)
-        return self.get().text
+        return self.get()
 
     def get_donations_for_user(self, email, password):
         self.build_authentication(email, password)
         self.api_endpoint = '/[appId]/[apiVersion]/account/donations'
-        return self.get().text
+        return self.get()
 
     def retrieve_account(self, email, password):
         self.build_authentication(email, password)
         self.api_endpoint = '/[appId]/[apiVersion]/account'
-        return self.get().text
+        return self.get()
 
     def validate(self, email, password):
         data = {"email": email, "password": password}
         self.api_endpoint = '/[appId]/[apiVersion]/account/validate'
-        return self.post(data).text
+        return self.post(data)
 
 
 class FundraisingAPIClient(BaseAPIClient):
@@ -89,12 +99,12 @@ class FundraisingAPIClient(BaseAPIClient):
     def get_fundraising_pages(self, email, password):
         self.build_authentication(email, password)
         self.api_endpoint = '/[appId]/[apiVersion]/fundraising/pages'
-        return self.get().text
+        return self.get()
 
     def get_fundraising_page_details(self, page_short_name):
         self.api_endpoint = '/[appId]/[apiVersion]/fundraising/pages/{0}'.format(
             page_short_name)
-        return self.get().text
+        return self.get()
 
     # If email and password not set, retunrs public data only
     def get_fundraising_page_donations(self, page_short_name, page_num=1, page_size=25, email=None, password=None):
@@ -103,7 +113,7 @@ class FundraisingAPIClient(BaseAPIClient):
 
         self.api_endpoint = '/[appId]/[apiVersion]/fundraising/pages/{0}/donations?pageNum={1}&pageSize={2}'.format(
             page_short_name, page_num, page_size)
-        return self.get().text
+        return self.get()
 
     def fundraising_page_url_check(self, page_short_name):
 
@@ -112,6 +122,7 @@ class FundraisingAPIClient(BaseAPIClient):
         return self.head()
 
 if __name__ == '__main__':
+    from pprint import pprint
     appID = '196e4994'
     j = JustGivingAPI(appID)
     # SAMPLE Check if test account exist
@@ -119,9 +130,9 @@ if __name__ == '__main__':
     # SAMPLE GET fundraising page
     # print j.fundraising.get_fundraising_pages('ching.leung@bynd.com', 'oaktree99')
     # SAMPLE GET fundraising page deatils
-    print j.fundraising.get_fundraising_page_details('Nicholas-Jones16')
+    pprint(j.fundraising.get_fundraising_page_details('Nicholas-Jones16'))
     # SAMPLE read donations on one particalar page, with page size of 150
     # result
-    print j.fundraising.get_fundraising_page_donations('Nicholas-Jones16', 1, 150)
+    #print j.fundraising.get_fundraising_page_donations('Nicholas-Jones16', 1, 150)
     # Check if justgiving donation page exist
     # print j.fundraising.fundraising_page_url_check('micwong')
